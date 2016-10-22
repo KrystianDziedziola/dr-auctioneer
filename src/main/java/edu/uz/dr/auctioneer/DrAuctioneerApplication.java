a84494a32e9ec8c18db64b249d3bbe73c7a1dce9
piotr_rozanski <87191@stud.uz.zgora.pl>
2016-10-22 23:33:51 +0200

    #3oddanie i skonfigurowanie Logbacka

--- src/main/java/edu/uz/dr/auctioneer/DrAuctioneerApplication.java
+++ src/main/java/edu/uz/dr/auctioneer/DrAuctioneerApplication.java
@@ -1,12 +1,14 @@
 package edu.uz.dr.auctioneer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DrAuctioneerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DrAuctioneerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DrAuctioneerApplication.class, args);
+    }
 }
