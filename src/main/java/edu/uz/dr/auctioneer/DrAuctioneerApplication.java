3c350be1f0d712c9e3e05b3d18c669bfe6dfc701
Krystian Dziędzioła <krystian.dziedziola@gmail.com>
2016-10-22 16:51:08 +0200

    AUCTIONEER-2-dodanie-projektu-spring-boot

---
+++ src/main/java/edu/uz/dr/auctioneer/DrAuctioneerApplication.java
@@ -0,0 +1,12 @@
+package edu.uz.dr.auctioneer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrAuctioneerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrAuctioneerApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/uz/dr/auctioneer/DrAuctioneerApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.uz.dr.auctioneer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrAuctioneerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
