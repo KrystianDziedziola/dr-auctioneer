b79210625efb25363be0348d2640619c42026c0b
piotr_rozanski <87191@stud.uz.zgora.pl>
2016-10-25 21:12:38 +0200

    #3 poprawienie

--- src/main/java/edu/uz/dr/auctioneer/DrAuctioneerApplication.java
+++ src/main/java/edu/uz/dr/auctioneer/DrAuctioneerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DrAuctioneerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DrAuctioneerApplication.class, args);
 	}
