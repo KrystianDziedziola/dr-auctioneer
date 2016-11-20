7078ea13a5011c4925fd469e2ec6200a7015da09
Krystian Dziędzioła <krystian.dziedziola@gmail.com>
2016-11-20 01:44:13 +0100

    AUCTIONEER-21 dodanie prostego serwisu do zarządania aukcjami + próbny kontroler

---
+++ src/test/java/edu/uz/dr/auctioneer/service/AuctionServiceTest.java
@@ -0,0 +1,41 @@
+package edu.uz.dr.auctioneer.service;
+
+import edu.uz.dr.auctioneer.model.auction.Auction;
+import edu.uz.dr.auctioneer.persistence.AuctionRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuctionServiceTest {
+
+    private AuctionService service;
+
+    @Mock
+    private AuctionRepository repository;
+
+    private Auction auction;
+
+    @Before
+    public void Set_Up() {
+        service = new AuctionService(repository);
+        auction = new Auction.Builder().setTitle("Title").build();
+    }
+
+    @Test
+    public void Should_Add_Auction() {
+        // given
+
+        // when
+        service.add(auction);
+
+        // then
+        verify(repository, times(1)).save(any(Auction.class));
+    }
+}
